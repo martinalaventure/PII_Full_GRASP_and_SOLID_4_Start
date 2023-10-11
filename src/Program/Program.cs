cef7be6b460f7d1c06a198707ef587bf56ed463f
Martina Laventure <martina.laventure@correo.ucu.edu.uy>
2023-10-11 10:27:04 -0300

    terminado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -22,11 +22,12 @@ namespace Full_GRASP_And_SOLID
 
             Recipe recipe = new Recipe();
             recipe.FinalProduct = GetProduct("Café con leche");
-            recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
-            recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
+
+            recipe.AddStep(GetProduct("Café"),100,GetEquipment("Cafetera"),120);
+            recipe.AddStep(GetProduct("Leche"),200,GetEquipment("Hervidor"),60);
 
             IPrinter printer;
-            printer = new ConsolePrinter();
+            printer= new ConsolePrinter();
             printer.PrintRecipe(recipe);
             printer = new FilePrinter();
             printer.PrintRecipe(recipe);
